176930dc8f7dd1e5aa72d7889143ab6c342fbeac
Antone Evans <antoneev65@gmail.com>
2020-08-29 12:09:36 -0500

    Final Project

--- README.md
+++ README.md
@@ -5,20 +5,21 @@
 
 I decided to build this program to continue building my knowledge. 
 
-Dataset
+#### Dataset
 Images in my dataset were gathered by Google using the Fatkun plugin; however, the downfall with this plugin is you'll have to click download N times for each image you chose. 
 In addition, to increase my dataset I used Downloader for Instagram to download bulk images from each athletes Instagram.
 
-Data Exploration and Cleaning 
+#### Data Exploration and Cleaning 
 The project uses OpenCV to find the eyes and face of each image. Wavelet is used to give each image depth by breaking the image into segments by feature to give us more information. 
 After all images were cropped, in the cropped folder I manually deleted images which were not of the intended athlete. This would happen if the image had multiple individuals within it. 
 
-Training 
+#### Data Training 
 I used GridSearchCV to tune the parameters. I then got the best estimator which in my case was SVM. You'll notice that my model didn't do very good during training or testing, this is expected as the dataset was very small.
 
 Data Visualization of our SVM Model
 ![Image of Data Visualization]()
 
+#### Backend and Frontend
 I then created a Python Flask model to run my model on and connect it to the front-end using HTML which uses CSS and JS to connect the UI and Flask server. 
 
 ![Image of Initial App ]()
---
+++ client/app.js
@@ -0,0 +1,74 @@
+Dropzone.autoDiscover = false;
+
+function init() {
+    let dz = new Dropzone("#dropzone", {
+        url: "/",
+        maxFiles: 1,
+        addRemoveLinks: true,
+        dictDefaultMessage: "Some Message",
+        autoProcessQueue: false
+    });
+    
+    dz.on("addedfile", function() {
+        if (dz.files[1]!=null) {
+            dz.removeFile(dz.files[0]);        
+        }
+    });
+
+    dz.on("complete", function (file) {
+        let imageData = file.dataURL;
+        
+        var url = "http://127.0.0.1:5000/classify_image";
+
+        $.post(url, {
+            image_data: imageData
+        },function(data, status) { 
+            console.log(data);
+            /* Display error message if face was not detected */
+            if (!data || data.length==0) {
+                $("#resultHolder").hide();
+                $("#divClassTable").hide();                
+                $("#error").show();
+                return;
+            }
+            let players = ["jonquel_jones", "steven_gardiner", "shaunae_miller-uibo", "buddy_hield"];
+            /* Display the player with the highest probability if multiple faces in an image */
+            let match = null;
+            let bestScore = -1;
+            for (let i=0;i<data.length;++i) {
+                let maxScoreForThisClass = Math.max(...data[i].class_probability);
+                if(maxScoreForThisClass>bestScore) {
+                    match = data[i];
+                    bestScore = maxScoreForThisClass;
+                }
+            }
+            if (match) {
+                $("#error").hide();
+                $("#resultHolder").show();
+                $("#divClassTable").show();
+                $("#resultHolder").html($(`[data-player="${match.class}"`).html());
+                let classDictionary = match.class_dictionary;
+                for(let personName in classDictionary) {
+                    let index = classDictionary[personName];
+                    let proabilityScore = match.class_probability[index];
+                    let elementName = "#score_" + personName;
+                    $(elementName).html(proabilityScore);
+                }
+            }
+            dz.removeFile(file);            
+        });
+    });
+
+    $("#submitBtn").on('click', function (e) {
+        dz.processQueue();		
+    });
+}
+
+$(document).ready(function() {
+    console.log( "ready!" );
+    $("#error").hide();
+    $("#resultHolder").hide();
+    $("#divClassTable").hide();
+
+    init();
+});
